db6c129771838d8788ec4f2e8bf3d305a296af55
CPerezz <c.perezbaro@gmail.com>
2020-05-07 12:16:09 +0200

    Modify lib structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 //! Reference implementation for the Poseidon Sponge function
-#![feature(external_doc)]
 #![deny(missing_docs)]
-#![doc(include = "../README.md")]
 
 /// Reference implementation for the Poseidon Sponge function
 pub mod sponge;
