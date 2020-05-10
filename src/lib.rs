57fce993106a6d0e6daa575ab7c4507d622c854b
CPerezz <c.perezbaro@gmail.com>
2020-05-10 19:25:30 +0200

    Update lib modules & update Kelvin dep

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 //! Reference implementation for the Poseidon Sponge function
 #![deny(missing_docs)]
 
-/// Reference implementation for the Poseidon Sponge function
+/// Reference implementation for the Poseidon Merkle hash function
+pub mod merkle_lvl_hash;
+/// Reference implementation for the gadget that builds a merkle opening proof
+pub mod merkle_proof;
+/// Reference implementation for the Poseidon Sponge hash function
 pub mod sponge;
---
+++ src/merkle_proof/mod.rs
@@ -0,0 +1,2 @@
+pub(crate) mod poseidon_branch;
+pub(crate) mod proof;
