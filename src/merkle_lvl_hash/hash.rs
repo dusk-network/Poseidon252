a3c0a83fe04a1a35ed6b68e187f479c389cfb9ef
CPerezz <carlos@dusk.network>
2020-05-12 12:23:08 +0200

    Refactored hash module w/ bitflag dependant fn's
    The goal of this library is to provide a way to easily
    generate Merkle Tree Proofs inside of ZK Constraint Systems.
    
    But to grant some level of freedom to the user, we've splited
    the implementation of the merkle-level hashing logic in 4 functions:
    
    - Implemented Scalar Merkle-tree-level hashing for levels with and
    without the bitflags coefficient.
    
    - Implemented Variable Merkle-Tree-level hashing gadget for levels with
    and without the bitflags coefficient.
    
    Closes #9

--- src/merkle_lvl_hash/hash.rs
+++ src/merkle_lvl_hash/hash.rs
@@ -1,9 +1,10 @@
+use crate::merkle_proof::poseidon_branch::PoseidonLevel;
 use dusk_bls12_381::Scalar;
 use dusk_plonk::constraint_system::{StandardComposer, Variable};
 use hades252::strategies::*;
 use hades252::WIDTH;
 
-/// The `poseidon_hash` function takes a Merkle Tree level with up to `ARITY`
+/// The `poseidon_hash` function takes a Merkle Tree Level with up to `ARITY`
 /// leaves and applies the poseidon hash, using the `hades252::ScalarStragegy` and
 /// computing the corresponding bitflags.
 pub fn merkle_level_hash(leaves: &[Scalar]) -> Scalar {
@@ -20,6 +21,15 @@ pub fn merkle_level_hash(leaves: &[Scalar]) -> Scalar {
         });
     // Set bitflags as first element.
     res[0] = Scalar::from(accum);
+    strategy.perm(&mut res);
+    res[1]
+}
+
+/// The `poseidon_hash` function takes a `PoseidonLevel` which has already computed
+/// the bitflags and hashes it returning the resulting `Scalar`.
+pub(crate) fn merkle_level_hash_without_bitflags(poseidon_level: &PoseidonLevel) -> Scalar {
+    let mut strategy = ScalarStrategy::new();
+    let mut res = poseidon_level.leaves.clone();
     strategy.perm(&mut res);
     res[1]
 }
@@ -43,4 +53,16 @@ pub fn merkle_level_hash_gadget(composer: &mut StandardComposer, leaves: &[Varia
     let mut strategy = GadgetStrategy::new(composer);
     strategy.perm(&mut res);
     res[1]
+}
+
+/// The `poseidon_hash` function takes a Merkle Tree level which has
+/// already computed the bitflags term and applies the poseidon hash,
+/// using the `hades252::GadgetStragegy` returning the resulting `Variable`.
+pub fn merkle_level_hash_gadget_without_bitflags(
+    composer: &mut StandardComposer,
+    leaves: &mut [Variable; WIDTH],
+) -> Variable {
+    let mut strategy = GadgetStrategy::new(composer);
+    strategy.perm(leaves);
+    leaves[1]
 }
