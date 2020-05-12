254a152aa4c580d6691d8ff776f5daf6be0d0b96
CPerezz <carlos@dusk.network>
2020-05-12 12:32:31 +0200

    Move ARITY param to lib const definition

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,17 @@
 //! Reference implementation for the Poseidon Sponge function
 #![deny(missing_docs)]
 
+/// Helpers for kelvin hashing & storing trait implementations
+pub mod hashing_utils;
 /// Reference implementation for the Poseidon Merkle hash function
 pub mod merkle_lvl_hash;
 /// Reference implementation for the gadget that builds a merkle opening proof
 pub mod merkle_proof;
 /// Reference implementation for the Poseidon Sponge hash function
 pub mod sponge;
+
+/// Maximum arity supported for trees.
+///
+/// This is due to the fact that actually we rely in Hades252 crate
+/// which `WIDTH` parameter is 5.
+pub const ARITY: usize = hades252::WIDTH - 1;
