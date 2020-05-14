d312495c5668eebcd13487e16af3c6518ecff571
CPerezz <carlos@dusk.network>
2020-05-14 17:19:22 +0200

    Add ScalarStorage docs

--- src/hashing_utils/scalar_storage.rs
+++ src/hashing_utils/scalar_storage.rs
@@ -4,16 +4,35 @@
 use dusk_bls12_381::Scalar;
 use kelvin::{ByteHash, Content, Sink, Source};
 use std::io;
-use std::io::Read;
+use std::io::{BufWriter, Read, Write};
 
 #[derive(Debug, Clone)]
-pub(crate) struct StorageScalar(pub(crate) Scalar);
+/// This struct is a Wrapper type over the bls12-381 `Scalar` which has implemented
+/// inside the logic to allows `Kelvin` Merkle Trees understand how to store `Scalar`s
+/// inside of them leaves.
+///
+/// This Struct is the one that we will use inside of our SmartContract storage logic to
+/// encode/compress all of our Data Structures data into a single `Scalar`.
+pub struct StorageScalar(pub(crate) Scalar);
 
-impl From<Scalar> for StorageScalar {
-    fn from(scalar: Scalar) -> StorageScalar {
-        StorageScalar(scalar)
+impl Into<Scalar> for StorageScalar {
+    fn into(self) -> Scalar {
+        self.0
     }
 }
+
+/*impl<T> From<T> for StorageScalar
+where
+    T: Write,
+{
+    fn from(t: T) -> Self {
+        let mut bytes = BufWriter::new(t);
+        let len = bytes.iter();
+
+        // Define Dmitry's logic to encode any data structure as a Scalar.
+        unimplemented!()
+    }
+}*/
 
 // Implements logic for storing Scalar inside of kelvin
 impl<H> Content<H> for StorageScalar
