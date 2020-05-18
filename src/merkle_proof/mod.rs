56c6cf861dd176d4828afb69d1b72323e7911a9e
CPerezz <carlos@dusk.network>
2020-05-18 17:34:40 +0200

    Implement Merkle Opening proof generators &verif
    **Merkle Proof Opening Gadget**
    
    Provided a `PoseidonBranch`, this method takes a `&mut StandardComposer`
    and performs the addition of the needed constraints into the Composer
    so it can demonstrate that the path to the root is correct.
    
    - Root is used as a PublicInput so the `Verifier` can check that
    indeed he/she is verifying the path to the correct root and de-facto
    the correct tree.
    
    **Merkle Proof Opening Scalar Verification**
    
    The method enables the capability of checking merkle path
    proofs using Scalars.
    
    Closes #10.

--- src/merkle_proof/mod.rs
+++ src/merkle_proof/mod.rs
@@ -1,2 +1,5 @@
 pub(crate) mod poseidon_branch;
+pub(crate) mod proof;
+
 pub use poseidon_branch::{PoseidonBranch, PoseidonLevel};
+pub use proof::{merkle_opening_gadget, merkle_opening_scalar_verification};
