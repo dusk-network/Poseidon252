b3cd0341ee9cd39b112666c482c2c984f40b2f55
CPerezz <carlos@dusk.network>
2020-05-18 17:08:58 +0200

    Implement PoseidonBranch & PoseidonLevel
    The `PoseidonBranch` is the data structure over which
    we will build our opening proofs.
    
    - Implement `From<kelvin::Branch> for PoseidonBranch.
    - Implement `PoseidonLevel` structure.
    - Add docs for every structure.
    
    These two allow us to obtain in the correct structure
    the data obtained from a `kelvin` search to then elaborate
    a merkle-opening proof.
    
    We needed a way to collect the data from the kelvin `Branch` and
    store it on a way which allows an easy Proof generation on later
    stages.
    
    The `From` trait implementation takes care of iterating over the Branch
    levels and so, collect all of the data while also computes the bitflags
    terms for each one of the leves. It also stores the index of the
    position of the upper level hash to link the results and chain up the
    hashing procedure until the root of the tree.
    
    - Implemented also a few extra methods that allow to generate default
    empty poseidon branches and levels so we have easier ways to generate
    the `PoseidonBranch` in the `From` impl.
    
    - Move ARITY definition to `lib.rs`.
    - Add some module-docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
 //! Reference implementation for the Poseidon Sponge function
 #![deny(missing_docs)]
 
-///
+/// Reference implementation for the Poseidon Merkle hash function
 pub mod merkle_lvl_hash;
-/// Reference implementation for the Poseidon Sponge function
+/// Reference implementation for the gadget that builds a merkle opening proof
+pub mod merkle_proof;
+/// Reference implementation for the Poseidon Sponge hash function
 pub mod sponge;
+
+/// Maximum arity supported for trees.
+///
+/// This is due to the fact that actually we rely in Hades252 crate
+/// which `WIDTH` parameter is 5.
+pub const ARITY: usize = hades252::WIDTH - 1;
---
+++ src/merkle_proof/poseidon_branch.rs
@@ -0,0 +1,134 @@
+//! Definitions of the merkle tree structure seen in Poseidon.
+
+use crate::hashing_utils::scalar_storage::StorageScalar;
+use crate::ARITY;
+use dusk_bls12_381::Scalar;
+use hades252::WIDTH;
+use kelvin::{Branch, ByteHash, Compound};
+use std::borrow::Borrow;
+
+/// The `Poseidon` structure will accept a number of inputs equal to the arity.
+///
+/// The levels are ordered so the first element of `levels` is actually the bottom
+/// level of the Kelvin tree.
+#[derive(Debug, Clone, PartialEq)]
+pub struct PoseidonBranch {
+    /// Root of the Merkle Tree
+    pub root: Scalar,
+    /// Levels of the MerkleTree with it's corresponding leaves and offset.
+    pub levels: Vec<PoseidonLevel>,
+}
+
+/// Provides a conversion between Branch and PoseidonBranch.
+///
+/// We extract the data from the `Branch` and store it appropiately
+/// inside of the `PoseidonBranch` structure with the bitflags already
+/// computed and the offsets pointing to the next levels pointing also to
+/// the correct places.
+impl<C, H> From<&Branch<'_, C, H>> for PoseidonBranch
+where
+    C: Compound<H>,
+    C::Annotation: Borrow<StorageScalar>,
+    H: ByteHash,
+{
+    fn from(branch: &Branch<C, H>) -> PoseidonBranch {
+        let mut poseidon_branch = PoseidonBranch::with_capacity(branch.levels().len() - 1);
+        // Skip root and store it directly.
+        poseidon_branch.root = branch
+            .levels()
+            .first()
+            .unwrap()
+            .annotation()
+            .unwrap()
+            .borrow()
+            .to_owned()
+            .into();
+        // Store the levels with the bitflags already computed inside
+        // of our PoseidonBranch structure.
+        for level in branch.levels().iter().rev() {
+            // Generate a default mutable `PoseidonLevel`, add the corresponding data
+            // extracted from the `Branch` and push it to our poseidon branch previously
+            // generated.
+            poseidon_branch.levels.push({
+                let mut pos_level = PoseidonLevel::default();
+                let mut level_bitflags = 0u64;
+                level
+                    .children()
+                    .iter()
+                    // Copy in poseidon_branch the leave values of the actual level with an
+                    // offset of one. So then we can add the bitflags at the beggining as the
+                    // first item of the `WIDTH` ones.
+                    .zip(pos_level.leaves.iter_mut().skip(1))
+                    // If they're null, place a Scalar::zero() inside of them as stated on the
+                    // Poseidon Hash paper.
+                    .enumerate()
+                    .for_each(|(idx, (src, dest))| {
+                        *dest = match src.annotation() {
+                            Some(borrow) => {
+                                let annotation: &Scalar = &(*borrow).borrow().borrow();
+                                // If the Annotation contains a value, we set the bitflag to 1.
+                                // Since the first element will be the most significant bit of the
+                                // bitflags, we need to shift it according to the `ARITY`.
+                                //
+                                // So for example:
+                                // A level with: [Some(val), None, None, None] should correspond to
+                                // Bitflags(1000). This means that we need to shift the first element
+                                // by `ARITY` and lately decrease the shift order by `idx`.
+                                level_bitflags += 1u64 << ((ARITY - 1) - idx);
+                                *annotation
+                            }
+                            None => Scalar::zero(),
+                        };
+                    });
+                // Now we should have our bitflags value computed as well as the
+                // `WIDTH` leaves set on the [1..4] positions of our poseidon_level.
+                //
+                // We need now to add the bitflags element in pos_level.leaves[0]
+                pos_level.leaves[0] = Scalar::from(level_bitflags);
+                // Once we have the level, we get the position where the hash of the previous level is
+                // stored on the this level.
+                // NOTE that this position is in respect of a WIDTH = `ARITY` so we need to
+                // keep in mind that we've added an extra term (bitflags) and so, the index that
+                // the branch is returning is indeed pointing one position before on the next level
+                // that we will compute later. We just add 1 to it to inline the value with the
+                // new `WIDTH`
+                pos_level.offset = level.offset() + 1;
+                pos_level
+            })
+        }
+        poseidon_branch
+    }
+}
+
+impl PoseidonBranch {
+    /// Generates a default PoseidonBranch with the specified capacity for storing
+    /// `n` levels inside.
+    pub fn with_capacity(n: usize) -> Self {
+        PoseidonBranch {
+            root: Scalar::zero(),
+            levels: Vec::with_capacity(n),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+/// Represents a Merkle-Tree Level inside of a `PoseidonBranch`.
+/// It stores the leaves as `Scalar` and the offset which represents
+/// the position on the level where the hash of the previous `PoseidonLevel`
+/// is stored in.
+pub struct PoseidonLevel {
+    /// Position on the level where the hash of the previous `PoseidonLevel`
+    /// is stored in.
+    pub offset: usize,
+    /// Leaves of the Level.
+    pub leaves: [Scalar; WIDTH],
+}
+
+impl Default for PoseidonLevel {
+    fn default() -> Self {
+        PoseidonLevel {
+            offset: 0usize,
+            leaves: [Scalar::zero(); WIDTH],
+        }
+    }
+}
