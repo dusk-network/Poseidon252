cb62ce3df5537d5a29acbc546c189977a8918252
CPerezz <carlos@dusk.network>
2020-07-22 11:23:16 +0200

    Update sponge module to work with plonkv0.2.0
    Refactored the tests in order to work with the new
    Prover&Verifier abstraction.

--- src/sponge/sponge.rs
+++ src/sponge/sponge.rs
@@ -1,20 +1,19 @@
 //! The `pad` module implements the Sponge's padding algorithm
 use super::pad::*;
 
-use dusk_bls12_381::Scalar;
-use dusk_plonk::constraint_system::{StandardComposer, Variable};
+use dusk_plonk::prelude::*;
 use hades252::strategies::*;
 use hades252::WIDTH;
 
 /// The `hash` function takes an arbitrary number of Scalars and returns the
 /// hash, using the `Hades` ScalarStragegy
-pub fn sponge_hash(messages: &[Scalar]) -> Scalar {
+pub fn sponge_hash(messages: &[BlsScalar]) -> BlsScalar {
     let mut strategy = ScalarStrategy::new();
 
     // The value used to pad the words is zero.
-    let padder = Scalar::zero();
+    let padder = BlsScalar::zero();
     // One will identify the end of messages.
-    let eom = Scalar::one();
+    let eom = BlsScalar::one();
 
     let mut words = pad(messages, WIDTH, padder, eom);
     // If the words len is less than the Hades252 permutation `WIDTH` we directly
@@ -27,7 +26,7 @@ pub fn sponge_hash(messages: &[Scalar]) -> Scalar {
     // need to collapse the padded limbs. See bottom of pag. 16 of
     // https://eprint.iacr.org/2019/458.pdf
     words.chunks(WIDTH).fold(
-        vec![Scalar::zero(); WIDTH],
+        vec![BlsScalar::zero(); WIDTH],
         |mut inputs, values| {
             let mut values = values.iter();
             inputs
@@ -46,12 +45,15 @@ pub fn sponge_hash_gadget(
     composer: &mut StandardComposer,
     messages: &[Variable],
 ) -> Variable {
+    // Create and constrait one and zero as witnesses.
+    let zero = composer.add_input(BlsScalar::zero());
+    composer.constrain_to_constant(zero, BlsScalar::zero(), BlsScalar::zero());
+    let one = composer.add_input(BlsScalar::one());
+    composer.constrain_to_constant(one, BlsScalar::one(), BlsScalar::one());
     // The value used to pad the words is zero.
-    let padder = composer.zero_var;
+    let padder = zero;
     // One will identify the end of messages.
-    let eom = composer.add_input(Scalar::one());
-    // Constraint eom to actually be One.
-    composer.constrain_to_constant(eom, Scalar::one(), Scalar::zero());
+    let eom = one;
 
     let mut words = pad(messages, WIDTH, padder, eom);
     // If the words len is less than the Hades252 permutation `WIDTH` we directly
@@ -70,10 +72,10 @@ pub fn sponge_hash_gadget(
             let mut values = values.iter();
             inputs.iter_mut().for_each(|input| {
                 *input = composer.add(
-                    (Scalar::one(), *input),
-                    (Scalar::one(), *values.next().unwrap()),
-                    Scalar::zero(),
-                    Scalar::zero(),
+                    (BlsScalar::one(), *input),
+                    (BlsScalar::one(), *values.next().unwrap()),
+                    BlsScalar::zero(),
+                    BlsScalar::zero(),
                 )
             });
             let mut strategy = GadgetStrategy::new(composer);
@@ -87,31 +89,34 @@ pub fn sponge_hash_gadget(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use dusk_plonk::commitment_scheme::kzg10::PublicParameters;
-    use dusk_plonk::fft::EvaluationDomain;
-    use merlin::Transcript;
 
     const CAPACITY: usize = 1 << 12;
 
-    fn poseidon_sponge_params(width: usize) -> (Vec<Scalar>, Scalar) {
-        let mut input = vec![Scalar::zero(); width];
+    fn poseidon_sponge_params(width: usize) -> (Vec<BlsScalar>, BlsScalar) {
+        let mut input = vec![BlsScalar::zero(); width];
         input
             .iter_mut()
-            .for_each(|s| *s = Scalar::random(&mut rand::thread_rng()));
+            .for_each(|s| *s = BlsScalar::random(&mut rand::thread_rng()));
         let output = sponge_hash(&input);
         (input, output)
     }
 
     // Checks that the result of the hades permutation is the same as the one obtained by
     // the sponge gadget
-    fn new_composer(
+    fn sponge_gadget_tester(
         width: usize,
-        i: Vec<Scalar>,
-        out: Scalar,
-    ) -> StandardComposer {
-        let mut composer = StandardComposer::new();
+        i: Vec<BlsScalar>,
+        out: BlsScalar,
+        composer: &mut StandardComposer,
+    ) {
+        let zero = composer.add_input(BlsScalar::zero());
+        composer.constrain_to_constant(
+            zero,
+            BlsScalar::zero(),
+            BlsScalar::zero(),
+        );
 
-        let mut i_var = vec![composer.zero_var; width];
+        let mut i_var = vec![zero; width];
         i.iter().zip(i_var.iter_mut()).for_each(|(i, v)| {
             *v = composer.add_input(*i);
         });
@@ -125,16 +130,15 @@ mod tests {
         composer.add_gate(
             o_var,
             computed_o_var,
-            composer.zero_var,
-            Scalar::one(),
-            -Scalar::one(),
-            Scalar::zero(),
-            Scalar::zero(),
-            Scalar::zero(),
+            zero,
+            BlsScalar::one(),
+            -BlsScalar::one(),
+            BlsScalar::zero(),
+            BlsScalar::zero(),
+            BlsScalar::zero(),
         );
 
         composer.add_dummy_constraints();
-        composer
     }
 
     #[test]
@@ -143,26 +147,23 @@ mod tests {
         let public_parameters =
             PublicParameters::setup(CAPACITY, &mut rand::thread_rng()).unwrap();
         let (ck, vk) = public_parameters.trim(CAPACITY).unwrap();
-        let domain = EvaluationDomain::new(CAPACITY).unwrap();
 
         // Test with width = 3
-        let (i, o) = poseidon_sponge_params(3usize);
-        let mut composer = new_composer(3usize, i, o);
-        composer.check_circuit_satisfied();
-        let mut transcript = Transcript::new(b"testing");
-        // Preprocess circuit
-        let circuit = composer.preprocess(&ck, &mut transcript, &domain);
 
-        // Prove
-        let proof = composer.prove(&ck, &circuit, &mut transcript.clone());
+        // Proving
+        let (i, o) = poseidon_sponge_params(3usize);
+        let mut prover = Prover::new(b"sponge_tester");
+        sponge_gadget_tester(3usize, i, o, prover.mut_cs());
+        prover.preprocess(&ck).expect("Error on preprocessing");
+        let proof = prover.prove(&ck).expect("Error on proof generation");
 
         // Verify
-        assert!(proof.verify(
-            &circuit,
-            &mut transcript.clone(),
-            &vk,
-            &composer.public_inputs()
-        ));
+        let mut verifier = Verifier::new(b"sponge_tester");
+        sponge_gadget_tester(3usize, i, o, verifier.mut_cs());
+        verifier.preprocess(&ck).expect("Error on preprocessing");
+        assert!(verifier
+            .verify(&proof, &vk, &vec![BlsScalar::zero()])
+            .is_ok());
     }
 
     #[test]
@@ -171,54 +172,47 @@ mod tests {
         let public_parameters =
             PublicParameters::setup(CAPACITY, &mut rand::thread_rng()).unwrap();
         let (ck, vk) = public_parameters.trim(CAPACITY).unwrap();
-        let domain = EvaluationDomain::new(CAPACITY).unwrap();
 
         // Test with width = 5
-        let (i, o) = poseidon_sponge_params(WIDTH);
-        let mut composer = new_composer(WIDTH, i, o);
-        composer.check_circuit_satisfied();
-        let mut transcript = Transcript::new(b"testing");
-        // Preprocess circuit
-        let circuit = composer.preprocess(&ck, &mut transcript, &domain);
 
-        // Prove
-        let proof = composer.prove(&ck, &circuit, &mut transcript.clone());
+        // Proving
+        let (i, o) = poseidon_sponge_params(WIDTH);
+        let mut prover = Prover::new(b"sponge_tester");
+        sponge_gadget_tester(WIDTH, i, o, prover.mut_cs());
+        prover.preprocess(&ck).expect("Error on preprocessing");
+        let proof = prover.prove(&ck).expect("Error on proof generation");
 
         // Verify
-        assert!(proof.verify(
-            &circuit,
-            &mut transcript.clone(),
-            &vk,
-            &composer.public_inputs()
-        ));
+        let mut verifier = Verifier::new(b"sponge_tester");
+        sponge_gadget_tester(WIDTH, i, o, verifier.mut_cs());
+        verifier.preprocess(&ck).expect("Error on preprocessing");
+        assert!(verifier
+            .verify(&proof, &vk, &vec![BlsScalar::zero()])
+            .is_ok());
     }
 
     #[test]
     fn sponge_gadget_width_15() {
         // Setup OG params.
         let public_parameters =
-            PublicParameters::setup(CAPACITY * 8, &mut rand::thread_rng())
-                .unwrap();
-        let (ck, vk) = public_parameters.trim(CAPACITY * 8).unwrap();
-        let domain = EvaluationDomain::new(CAPACITY * 8).unwrap();
+            PublicParameters::setup(CAPACITY, &mut rand::thread_rng()).unwrap();
+        let (ck, vk) = public_parameters.trim(CAPACITY).unwrap();
 
         // Test with width = 15
-        let (i, o) = poseidon_sponge_params(15usize);
-        let mut composer = new_composer(15usize, i, o);
-        composer.check_circuit_satisfied();
-        let mut transcript = Transcript::new(b"testing");
-        // Preprocess circuit
-        let circuit = composer.preprocess(&ck, &mut transcript, &domain);
 
-        // Prove
-        let proof = composer.prove(&ck, &circuit, &mut transcript.clone());
+        // Proving
+        let (i, o) = poseidon_sponge_params(15usize);
+        let mut prover = Prover::new(b"sponge_tester");
+        sponge_gadget_tester(15usize, i, o, prover.mut_cs());
+        prover.preprocess(&ck).expect("Error on preprocessing");
+        let proof = prover.prove(&ck).expect("Error on proof generation");
 
         // Verify
-        assert!(proof.verify(
-            &circuit,
-            &mut transcript.clone(),
-            &vk,
-            &composer.public_inputs()
-        ));
+        let mut verifier = Verifier::new(b"sponge_tester");
+        sponge_gadget_tester(15usize, i, o, verifier.mut_cs());
+        verifier.preprocess(&ck).expect("Error on preprocessing");
+        assert!(verifier
+            .verify(&proof, &vk, &vec![BlsScalar::zero()])
+            .is_ok());
     }
 }
