5fcbd43db654abc7bc226cbe25aa7258e9936d58
Victor Lopez <victor@dusk.network>
2020-10-01 13:32:15 +0200

    Fix license header

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -1,9 +1,8 @@
-// Copyright (c) DUSK NETWORK. All rights reserved.
-// Licensed under the MPL 2.0 license. See LICENSE file in the project root for details.”
-//
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
 
 use std::borrow::Borrow;
 use std::io;
