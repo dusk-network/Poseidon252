dusk-network/zk-tools

Merkle opening constant circuit description (#123)
PLONK requires the circuit description + witness indexes to be constant
so the same verifier data can be reused to check different proofs.

The previous implementation contained an optimization that swaps the
witness index to reduce the number of gates.

This optimization may lead to inconsistencies when reusing verifier keys
because the verification will depend on the disposition of the witness
data, and this is undesirable.

Resolves #122

crates/poseidon/src/tree/zk.rs
@@ -12,7 +12,6 @@ use dusk_plonk::prelude::*;
 pub fn merkle_opening<const DEPTH: usize>(
     composer: &mut StandardComposer,
     branch: &PoseidonBranch<DEPTH>,
-    leaf: Variable,
 ) -> Variable {
     // Generate and constraint zero.
     let zero = composer.add_witness_to_circuit_description(BlsScalar::zero());
@@ -24,21 +23,61 @@ pub fn merkle_opening<const DEPTH: usize>(
     // For every level, replace the level offset with needle,
     // permutate the level and set the needle to the next level
     // to the poseidon result of the permutation
-    branch.as_ref().iter().fold(leaf, |needle, level| {
-        let offset = level.offset() as usize;
+    branch.as_ref().iter().for_each(|level| {
+        // Create the bits representation of the offset as witness
+        let offset_flag = level.offset_flag();
+        let mut sum = zero;
+        let mut offset_bits = [zero; dusk_hades::WIDTH - 1];
+        offset_bits.iter_mut().fold(1, |mask, bit| {
+            *bit = composer
+                .add_input(BlsScalar::from((offset_flag & mask).min(1)));
 
-        level.as_ref().iter().enumerate().for_each(|(i, l)| {
-            if i != offset {
-                perm[i] = composer.add_input(*l);
-            }
+            sum = composer.add(
+                (BlsScalar::one(), sum),
+                (BlsScalar::one(), *bit),
+                BlsScalar::zero(),
+                None,
+            );
+
+            mask << 1
         });
+        composer.constrain_to_constant(sum, BlsScalar::one(), None);
+
+        let leaf = composer.add_input(**level);
+        level
+            .as_ref()
+            .iter()
+            .zip(perm.iter_mut())
+            .enumerate()
+            .for_each(|(i, (l, p))| {
+                *p = composer.add_input(*l);
+
+                if i > 0 {
+                    let b = offset_bits[i - 1];
+
+                    let a = composer.mul(
+                        BlsScalar::one(),
+                        b,
+                        *p,
+                        BlsScalar::zero(),
+                        None,
+                    );
+
+                    let b = composer.mul(
+                        BlsScalar::one(),
+                        b,
+                        leaf,
+                        BlsScalar::zero(),
+                        None,
+                    );
+
+                    composer.assert_equal(a, b);
+                }
+            });
 
         root = perm[1];
-        perm[offset] = needle;
-
         let mut h = GadgetStrategy::new(composer);
         h.perm(&mut perm);
-        perm[1]
     });
 
     root
@@ -47,68 +86,98 @@ pub fn merkle_opening<const DEPTH: usize>(
 #[cfg(test)]
 mod tests {
     use crate::tree::tests::MockLeaf;
-    use crate::tree::{merkle_opening, PoseidonAnnotation, PoseidonTree};
-    use anyhow::Result;
+    use crate::tree::{self, PoseidonAnnotation, PoseidonBranch, PoseidonTree};
     use canonical_host::MemStore;
+    use dusk_plonk::circuit;
+    use dusk_plonk::prelude::Error as PlonkError;
     use dusk_plonk::prelude::*;
+    use rand::rngs::StdRng;
+    use rand::SeedableRng;
+    use rand::{CryptoRng, RngCore};
 
-    #[test]
-    fn tree_merkle_opening() -> Result<()> {
-        const DEPTH: usize = 17;
+    const DEPTH: usize = 17;
+    const CAPACITY: usize = 1 << 15;
+    type Tree = PoseidonTree<MockLeaf, PoseidonAnnotation, MemStore, DEPTH>;
 
-        let pub_params =
-            PublicParameters::setup(1 << 15, &mut rand::thread_rng())?;
-        let (ck, ok) = pub_params.trim(1 << 15)?;
+    struct MerkleOpeningCircuit {
+        branch: PoseidonBranch<DEPTH>,
+    }
 
-        let mut tree: PoseidonTree<
-            MockLeaf,
-            PoseidonAnnotation,
-            MemStore,
-            DEPTH,
-        > = PoseidonTree::new();
+    impl MerkleOpeningCircuit {
+        pub fn random<R: RngCore + CryptoRng>(
+            rng: &mut R,
+            tree: &mut Tree,
+        ) -> Self {
+            let leaf = MockLeaf::random(rng);
+            let pos = tree
+                .push(leaf.clone())
+                .expect("Failed to append to the tree");
 
-        for i in 0..1024 {
-            let l = MockLeaf::from(i as u64);
-            tree.push(l).unwrap();
+            let branch = tree
+                .branch(pos)
+                .expect("Failed to read the tree for the branch")
+                .expect("Failed to fetch the branch of the created leaf from the tree");
+
+            Self { branch }
         }
 
-        let gadget_tester = |composer: &mut StandardComposer,
-                             tree: &PoseidonTree<
-            MockLeaf,
-            PoseidonAnnotation,
-            MemStore,
-            DEPTH,
-        >,
-                             n: usize| {
-            let branch = tree.branch(n).unwrap().unwrap();
-            let root = tree.root().unwrap();
+        pub fn public_inputs(&self) -> Vec<PublicInputValue> {
+            vec![(*self.branch.root()).into()]
+        }
+    }
 
-            let leaf = BlsScalar::from(n as u64);
-            let leaf = composer.add_input(leaf);
+    impl Circuit for MerkleOpeningCircuit {
+        const CIRCUIT_ID: [u8; 32] = [0xff; 32];
 
-            let root_p = merkle_opening::<DEPTH>(composer, &branch, leaf);
+        fn gadget(
+            &mut self,
+            composer: &mut StandardComposer,
+        ) -> Result<(), PlonkError> {
+            let root = self.branch.root();
+            let root_p = tree::merkle_opening::<DEPTH>(composer, &self.branch);
+
             composer.constrain_to_constant(
                 root_p,
                 BlsScalar::zero(),
                 Some(-root),
             );
-        };
 
-        let label = b"opening_gadget";
-
-        for i in [0, 567, 1023].iter() {
-            let mut prover = Prover::new(label);
-            gadget_tester(prover.mut_cs(), &tree, *i);
-            prover.preprocess(&ck)?;
-            let proof = prover.prove(&ck)?;
-
-            let mut verifier = Verifier::new(label);
-            gadget_tester(verifier.mut_cs(), &tree, *i);
-            verifier.preprocess(&ck)?;
-            let pi = verifier.mut_cs().construct_dense_pi_vec();
-            verifier.verify(&proof, &ok, &pi).unwrap();
+            Ok(())
         }
 
-        Ok(())
+        fn padded_circuit_size(&self) -> usize {
+            CAPACITY
+        }
+    }
+
+    #[test]
+    fn tree_merkle_opening() {
+        let mut rng = StdRng::seed_from_u64(0xbeef);
+        let pp = PublicParameters::setup(CAPACITY, &mut rng).unwrap();
+        let label = b"dusk-network";
+
+        let mut tree = Tree::default();
+        let mut circuit = MerkleOpeningCircuit::random(&mut rng, &mut tree);
+        let (pk, vd) = circuit.compile(&pp).expect("Failed to compile circuit");
+
+        let mut tree = Tree::default();
+        for _ in 0..13 {
+            let mut circuit = MerkleOpeningCircuit::random(&mut rng, &mut tree);
+
+            let proof = circuit
+                .gen_proof(&pp, &pk, label)
+                .expect("Failed to generate proof");
+            let pi = circuit.public_inputs();
+
+            circuit::verify_proof(
+                &pp,
+                vd.key(),
+                &proof,
+                pi.as_slice(),
+                vd.pi_pos(),
+                label,
+            )
+            .expect("Proof verification failed");
+        }
     }
 }

dusk-network/Poseidon252

src/tree/zk.rs
@@ -12,7 +12,6 @@ use dusk_plonk::prelude::*;
 pub fn merkle_opening<const DEPTH: usize>(
     composer: &mut StandardComposer,
     branch: &PoseidonBranch<DEPTH>,
-    leaf: Variable,
 ) -> Variable {
     // Generate and constraint zero.
     let zero = composer.add_witness_to_circuit_description(BlsScalar::zero());
@@ -24,21 +23,61 @@ pub fn merkle_opening<const DEPTH: usize>(
     // For every level, replace the level offset with needle,
     // permutate the level and set the needle to the next level
     // to the poseidon result of the permutation
-    branch.as_ref().iter().fold(leaf, |needle, level| {
-        let offset = level.offset() as usize;
+    branch.as_ref().iter().for_each(|level| {
+        // Create the bits representation of the offset as witness
+        let offset_flag = level.offset_flag();
+        let mut sum = zero;
+        let mut offset_bits = [zero; dusk_hades::WIDTH - 1];
+        offset_bits.iter_mut().fold(1, |mask, bit| {
+            *bit = composer
+                .add_input(BlsScalar::from((offset_flag & mask).min(1)));
 
-        level.as_ref().iter().enumerate().for_each(|(i, l)| {
-            if i != offset {
-                perm[i] = composer.add_input(*l);
-            }
+            sum = composer.add(
+                (BlsScalar::one(), sum),
+                (BlsScalar::one(), *bit),
+                BlsScalar::zero(),
+                None,
+            );
+
+            mask << 1
         });
+        composer.constrain_to_constant(sum, BlsScalar::one(), None);
+
+        let leaf = composer.add_input(**level);
+        level
+            .as_ref()
+            .iter()
+            .zip(perm.iter_mut())
+            .enumerate()
+            .for_each(|(i, (l, p))| {
+                *p = composer.add_input(*l);
+
+                if i > 0 {
+                    let b = offset_bits[i - 1];
+
+                    let a = composer.mul(
+                        BlsScalar::one(),
+                        b,
+                        *p,
+                        BlsScalar::zero(),
+                        None,
+                    );
+
+                    let b = composer.mul(
+                        BlsScalar::one(),
+                        b,
+                        leaf,
+                        BlsScalar::zero(),
+                        None,
+                    );
+
+                    composer.assert_equal(a, b);
+                }
+            });
 
         root = perm[1];
-        perm[offset] = needle;
-
         let mut h = GadgetStrategy::new(composer);
         h.perm(&mut perm);
-        perm[1]
     });
 
     root
@@ -47,68 +86,98 @@ pub fn merkle_opening<const DEPTH: usize>(
 #[cfg(test)]
 mod tests {
     use crate::tree::tests::MockLeaf;
-    use crate::tree::{merkle_opening, PoseidonAnnotation, PoseidonTree};
-    use anyhow::Result;
+    use crate::tree::{self, PoseidonAnnotation, PoseidonBranch, PoseidonTree};
     use canonical_host::MemStore;
+    use dusk_plonk::circuit;
+    use dusk_plonk::error::Error as PlonkError;
     use dusk_plonk::prelude::*;
+    use rand::rngs::StdRng;
+    use rand::SeedableRng;
+    use rand::{CryptoRng, RngCore};
 
-    #[test]
-    fn tree_merkle_opening() -> Result<()> {
-        const DEPTH: usize = 17;
+    const DEPTH: usize = 17;
+    const CAPACITY: usize = 1 << 15;
+    type Tree = PoseidonTree<MockLeaf, PoseidonAnnotation, MemStore, DEPTH>;
 
-        let pub_params =
-            PublicParameters::setup(1 << 15, &mut rand::thread_rng())?;
-        let (ck, ok) = pub_params.trim(1 << 15)?;
+    struct MerkleOpeningCircuit {
+        branch: PoseidonBranch<DEPTH>,
+    }
 
-        let mut tree: PoseidonTree<
-            MockLeaf,
-            PoseidonAnnotation,
-            MemStore,
-            DEPTH,
-        > = PoseidonTree::new();
+    impl MerkleOpeningCircuit {
+        pub fn random<R: RngCore + CryptoRng>(
+            rng: &mut R,
+            tree: &mut Tree,
+        ) -> Self {
+            let leaf = MockLeaf::random(rng);
+            let pos = tree
+                .push(leaf.clone())
+                .expect("Failed to append to the tree");
 
-        for i in 0..1024 {
-            let l = MockLeaf::from(i as u64);
-            tree.push(l).unwrap();
+            let branch = tree
+                .branch(pos)
+                .expect("Failed to read the tree for the branch")
+                .expect("Failed to fetch the branch of the created leaf from the tree");
+
+            Self { branch }
         }
 
-        let gadget_tester = |composer: &mut StandardComposer,
-                             tree: &PoseidonTree<
-            MockLeaf,
-            PoseidonAnnotation,
-            MemStore,
-            DEPTH,
-        >,
-                             n: usize| {
-            let branch = tree.branch(n).unwrap().unwrap();
-            let root = tree.root().unwrap();
+        pub fn public_inputs(&self) -> Vec<PublicInputValue> {
+            vec![(*self.branch.root()).into()]
+        }
+    }
 
-            let leaf = BlsScalar::from(n as u64);
-            let leaf = composer.add_input(leaf);
+    impl Circuit for MerkleOpeningCircuit {
+        const CIRCUIT_ID: [u8; 32] = [0xff; 32];
 
-            let root_p = merkle_opening::<DEPTH>(composer, &branch, leaf);
+        fn gadget(
+            &mut self,
+            composer: &mut StandardComposer,
+        ) -> Result<(), PlonkError> {
+            let root = self.branch.root();
+            let root_p = tree::merkle_opening::<DEPTH>(composer, &self.branch);
+
             composer.constrain_to_constant(
                 root_p,
                 BlsScalar::zero(),
                 Some(-root),
             );
-        };
 
-        let label = b"opening_gadget";
-
-        for i in [0, 567, 1023].iter() {
-            let mut prover = Prover::new(label);
-            gadget_tester(prover.mut_cs(), &tree, *i);
-            prover.preprocess(&ck)?;
-            let proof = prover.prove(&ck)?;
-
-            let mut verifier = Verifier::new(label);
-            gadget_tester(verifier.mut_cs(), &tree, *i);
-            verifier.preprocess(&ck)?;
-            let pi = verifier.mut_cs().construct_dense_pi_vec();
-            verifier.verify(&proof, &ok, &pi).unwrap();
+            Ok(())
         }
 
-        Ok(())
+        fn padded_circuit_size(&self) -> usize {
+            CAPACITY
+        }
+    }
+
+    #[test]
+    fn tree_merkle_opening() {
+        let mut rng = StdRng::seed_from_u64(0xbeef);
+        let pp = PublicParameters::setup(CAPACITY, &mut rng).unwrap();
+        let label = b"dusk-network";
+
+        let mut tree = Tree::default();
+        let mut circuit = MerkleOpeningCircuit::random(&mut rng, &mut tree);
+        let (pk, vd) = circuit.compile(&pp).expect("Failed to compile circuit");
+
+        let mut tree = Tree::default();
+        for _ in 0..13 {
+            let mut circuit = MerkleOpeningCircuit::random(&mut rng, &mut tree);
+
+            let proof = circuit
+                .gen_proof(&pp, &pk, label)
+                .expect("Failed to generate proof");
+            let pi = circuit.public_inputs();
+
+            circuit::verify_proof(
+                &pp,
+                vd.key(),
+                &proof,
+                pi.as_slice(),
+                vd.pi_pos(),
+                label,
+            )
+            .expect("Proof verification failed");
+        }
     }
 }